4d0f10e79f9d94ec257517ce63fc6ebcb1cb5eb1
dhoker23 <44202889+dhoker23@users.noreply.github.com>
2019-04-17 17:25:16 +0400

    Update code-translator.js

--- code-translator.js
+++ code-translator.js
@@ -6,29 +6,29 @@ function inp(){
   
   var commandsByLang = {};
   commandsByLang['JavaScript'] = {}; 
-  commandsByLang['JavaScript'][0] = "alert('Hello, World!')";
-  commandsByLang['JavaScript'][1] = "var a = ''";
+  commandsByLang['JavaScript'][0] = "alert('Hello, World!');";
+  commandsByLang['JavaScript'][1] = "var a = '';";
   commandsByLang['C#'] = {}; 
   commandsByLang['C#'][0] = "Console.Write('Hello, World');";
-  commandsByLang['C#'][1] = "var a = ''"
+  commandsByLang['C#'][1] = "var a = '';"
   commandsByLang['PHP'] = {}; 
-  commandsByLang['PHP'][0] = "echo('Hello, World!')";
-  commandsByLang['PHP'][1] = "$ a = ''";
+  commandsByLang['PHP'][0] = "echo('Hello, World!');";
+  commandsByLang['PHP'][1] = "$ a = '';";
   commandsByLang['Ruby'] = {}; 
-  commandsByLang['Ruby'][0] = "puts 'Hello, World!'";
-  commandsByLang['Ruby'][1] = "@ a = ''";
+  commandsByLang['Ruby'][0] = "puts 'Hello, World!';";
+  commandsByLang['Ruby'][1] = "@ a = '';";
   commandsByLang['Python'] = {}; 
   commandsByLang['Python'][0] = "print 'Hello, World!'";
   commandsByLang['Python'][1] = "a = ''";
   commandsByLang['Pascal'] = {}; 
-  commandsByLang['Pascal'][0] = "Writeln('Hello, World!')";
-  commandsByLang['Pascal'][1] = "a:''";
+  commandsByLang['Pascal'][0] = "Writeln('Hello, World!');";
+  commandsByLang['Pascal'][1] = "a:'';";
   var commands = Object.values(commandsByLang[inp_lang]);
   var indexOfCommand = commands.indexOf(input_text);
   
   document.getElementById("out").value = commandsByLang[out_lang][indexOfCommand];
   
   if(indexOfCommand == "-1"){
-    alert("error");
+    document.getElementById("out").value = "извините,команда не распознана";
   }
 }
