cc08b2d249839af292a46d5992f64c7ec110a262
dhoker23 <44202889+dhoker23@users.noreply.github.com>
2019-04-17 16:52:18 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+﻿# code-translator
+доступные команды:
+
+PHP: echo('Hello, World!'),$ a = ''\\
+
+C#: Console.Write('Hello, World!'),string|int a = ''\\
+
+Ruby: puts 'Hello, World!',$/@ a = ''\\
+
+Python:print 'Hello, World!',a = ''\\
+
+Pascal:Writeln('Hello, World!'),a:''\\
+
+JavaScript:alert('Hello, World!'),var a = ''\\
---
+++ code-translator.js
@@ -0,0 +1,34 @@
+function inp(){
+    var input_text,inp_lang,out_lang;
+    input_text = document.getElementById("input").value;
+    inp_lang = document.getElementById("inplang").value;
+    out_lang = document.getElementById("out_lang").value;
+  
+  var commandsByLang = {};
+  commandsByLang['JavaScript'] = {}; 
+  commandsByLang['JavaScript'][0] = "alert('Hello, World!')";
+  commandsByLang['JavaScript'][1] = "var a = ''";
+  commandsByLang['C#'] = {}; 
+  commandsByLang['C#'][0] = "Console.Write('Hello, World');";
+  commandsByLang['C#'][1] = "string a = ''\nint a = ''"
+  commandsByLang['PHP'] = {}; 
+  commandsByLang['PHP'][0] = "echo('Hello, World!')";
+  commandsByLang['PHP'][1] = "$ a = ''";
+  commandsByLang['Ruby'] = {}; 
+  commandsByLang['Ruby'][0] = "puts 'Hello, World!'";
+  commandsByLang['Ruby'][1] = "$ a = ''\n@ a = ''";
+  commandsByLang['Python'] = {}; 
+  commandsByLang['Python'][0] = "print 'Hello, World!'";
+  commandsByLang['Python'][1] = "a = ''";
+  commandsByLang['Pascal'] = {}; 
+  commandsByLang['Pascal'][0] = "Writeln('Hello, World!')";
+  commandsByLang['Pascal'][1] = "a:''";
+  var commands = Object.values(commandsByLang[inp_lang]);
+  var indexOfCommand = commands.indexOf(input_text);
+  
+  document.getElementById("out").value = commandsByLang[out_lang][indexOfCommand];
+  
+  if(indexOfCommand == "-1"){
+    alert("error");
+  }
+}
